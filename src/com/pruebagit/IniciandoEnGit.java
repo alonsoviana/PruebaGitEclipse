70b432c882345faeac1174e26ae7a506a3c78923
Jose <elputoamo_822@hotmail.com>
2020-02-02 15:05:38 +0100

    Prueba de subida correcta

--- src/com/pruebagit/IniciandoEnGit.java
+++ src/com/pruebagit/IniciandoEnGit.java
@@ -3,7 +3,7 @@ package com.pruebagit;
 public class IniciandoEnGit {
 
 	public static void main(String[] args) {
-		System.out.println("Prueba de subida");
+		System.out.println("Prueba de subida correcta");
 
 	}
 
